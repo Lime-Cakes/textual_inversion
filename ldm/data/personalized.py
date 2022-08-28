734e2d556a94b3bc191a22a20e082fce48968755
Lime-Cakes <91322985+Lime-Cakes@users.noreply.github.com>
2022-08-28 18:12:38 +0800

    PIL image deprecated fix

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -113,10 +113,10 @@ class PersonalizedBase(Dataset):
             self._length = self.num_images * repeats
 
         self.size = size
-        self.interpolation = {"linear": PIL.Image.LINEAR,
-                              "bilinear": PIL.Image.BILINEAR,
-                              "bicubic": PIL.Image.BICUBIC,
-                              "lanczos": PIL.Image.LANCZOS,
+        self.interpolation = {"linear": PIL.Image.Resampling.LINEAR,
+                              "bilinear": PIL.Image.Resampling.BILINEAR,
+                              "bicubic": PIL.Image.Resampling.BICUBIC,
+                              "lanczos": PIL.Image.Resampling.ANCZOS,
                               }[interpolation]
         self.flip = transforms.RandomHorizontalFlip(p=flip_p)
 
